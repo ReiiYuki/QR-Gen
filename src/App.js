86ad70723ac8b24ba0bf8aa2f2f5ee2ab2bd3731
ReiiYuki <iguru.shou.5410070@gmail.com>
2019-01-10 22:23:12 +0700

    Complete QR Generator

--- src/App.js
+++ src/App.js
@@ -1,28 +1,125 @@
+import 'antd/dist/antd.css'
+
+import { Button, Card, Col, Input, Row } from 'antd';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import QrCodeWithLogo from 'qr-code-with-logo';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		imageUrl: '',
+		text: '',
+	}
+
+	qr = null
+
+	componentDidMount() {
+		this.updateDataByParams()
+	}
+
+	componentDidUpdate() {
+		this.updateQr()
+	}
+
+	updateDataByParams() {
+		const urlParams = new URLSearchParams(window.location.search);
+		const imageUrl = urlParams.get('imageUrl') || localStorage.getItem('imageUrl') || '';
+		const text = urlParams.get('text') || localStorage.getItem('text') || '';
+		this.setState({ imageUrl, text })
+	}
+
+	onChange = ({ target: { value, name }}) => {
+		this.setState({ [name] : value })
+		localStorage.setItem(name, value)
+	}
+
+	updateQr() {
+		const { imageUrl, text } = this.state
+		text && QrCodeWithLogo.toCanvas({
+			canvas: this.qr,
+			width: 480,
+			content: text,
+			logo: {
+				src: imageUrl,
+			}
+		})
+	}
+
+  	render() {
+		const { imageUrl, text } = this.state
+    	return (
+			<div style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center', 
+					height: '100vh',
+					width: '100vw',
+					background: 'gray',
+				}}
+			>
+				<Card style={{
+					width: '100%',
+					maxWidth: 900,
+					}}
+				>
+					<Row>
+						<Col span={24} style={{ display: 'flex', justifyContent: 'center', padding: 16 }}>
+							<canvas
+								ref={e => this.qr = e}
+								style={{
+									width: 480,
+									height: 480,
+									background: 'gray',
+								}}
+							/>
+						</Col>
+						<Col span={24}
+							style={{
+								padding: 16,
+								display: 'flex',
+								justifyContent: 'center',
+								flexDirection: 'column',
+							}}
+						>
+							<Input
+								name="text"
+								placeholder="Text / Url"
+								style={{
+									marginBottom: 16
+								}}
+								onChange={this.onChange}
+								value={text}
+							/>
+							<Input
+								name="imageUrl"
+								placeholder="Logo Url"
+								onChange={this.onChange}
+								value={imageUrl}
+								style={{
+									marginBottom: 16
+								}}
+							/>
+							<a
+								download="qr.png"
+								href={this.qr && this.qr.toDataURL('image/png')}
+							>
+								<Button
+									type="primary"
+									icon="download"
+									size="large"
+									style={{
+										width: '100%',
+									}}
+								>
+									Download
+								</Button>
+							</a>
+						</Col>
+					</Row>
+				</Card>
+			</div>
+    	);
+  	}
 }
 
 export default App;
